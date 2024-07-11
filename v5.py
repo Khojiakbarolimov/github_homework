68752c747e537b111f2b0957795120e77447e545
Khojiakbarolimov <olimovhojiakbar1206@gmail.com>
2024-07-11 21:05:07 +0500

    Update

--- v5.py
+++ v5.py
@@ -2,8 +2,17 @@ import os
 
 naqsh = []
 n = int(input("Kvadratlar soni: "))
-
+colours = ["Red", "Blue", "Yellow", "Green"]
 for i in range(n):
     os.system('cls')
     print(f"{i+1}-kvadrat bo'yash uchun kerak rang tanlang: ")
-    print("#1 Red\n#2 Blue\n#3 Yellow\n#4 Green\n")
+    inx = int(input("#1 Red\n#2 Blue\n#3 Yellow\n#4 Green\n"))-1
+    naqsh.append(colours[inx])
+count = 2
+for i in range(1, n):
+    if naqsh[i-1] != naqsh[i]:
+        count += 1
+    count += 2
+os.system('cls')
+print(f"naqsh({naqsh})")
+print(count)
